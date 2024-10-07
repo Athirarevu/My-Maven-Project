86ae4b38e9e6f7777f4d1a86c8c6892c8812af89
athir <athir@Athira>
2024-10-07 13:09:03 +0530

    MyFirstCommit

--- MyMavenWorks/src/main/java/access_Modifiers/Private2.java
+++ MyMavenWorks/src/main/java/access_Modifiers/Private2.java
@@ -5,8 +5,8 @@ public class Private2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Private1 obj=new Private1();
-		obj.show();                   // error, because its declared in class1 privately, it cannot access here
-		System.out.println(obj.a);    // error [instance var]
+		//obj.show();                   // error, because its declared in class1 privately, it cannot access here
+	//	System.out.println(obj.a);    // error [instance var]
 	}
 
 }
--- MyMavenWorks/src/main/java/access_modifier2/Default2.java
+++ MyMavenWorks/src/main/java/access_modifier2/Default2.java
@@ -4,8 +4,9 @@ public class Default2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Default1 obj=new Default1();
-		obj.show();                   // error, do not print output because Default1 is from different package
+	//	Default1 obj=new Default1();
+	//	obj.show();   
+		// error, do not print output because Default1 is from different package
 	}
 
 }
